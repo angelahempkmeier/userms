5d3737bbeea979f436b131a9d7ecbe537b5cfc4e
Angela Hempkmeier <aggsdpz@gmail.com>
2024-01-07 19:32:51 -0300

    feaat(repositories): UserRepository created

---
+++ src/main/java/com/ms/userms/repositories/UserRepository.java
@@ -0,0 +1,12 @@
+package com.ms.userms.repositories;
+
+import com.ms.userms.entities.UserEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.UUID;
+
+@Repository
+public interface UserRepository extends JpaRepository<UserEntity, UUID> {
+
+}
