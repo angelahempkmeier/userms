ab2cfc5adacfd63fea880c7c4720c7beb73d2524
Angela Hempkmeier <aggsdpz@gmail.com>
2024-01-07 19:20:53 -0300

    feat(entities): UserEntity created

---
+++ src/main/java/com/ms/userms/entities/UserEntity.java
@@ -0,0 +1,22 @@
+package com.ms.userms.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID userId;
+    private String name;
+    private String email;
+
+}
