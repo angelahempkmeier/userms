cb3f120637804a0f0b5999b0125de830030932dc
Angela Hempkmeier <aggsdpz@gmail.com>
2024-01-09 21:29:38 -0300

    feat(services): adding in the UserService class the publish message feature, in the saveUser method.

--- src/main/java/com/ms/userms/services/UserService.java
+++ src/main/java/com/ms/userms/services/UserService.java
@@ -3,6 +3,7 @@ package com.ms.userms.services;
 
 import com.ms.userms.dtos.UserDTO;
 import com.ms.userms.entities.UserEntity;
+import com.ms.userms.producers.UserProducer;
 import com.ms.userms.repositories.UserRepository;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.BeanUtils;
@@ -11,15 +12,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
     private UserRepository userRepository;
+    final UserProducer userProducer;
 
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, UserProducer userProducer) {
         this.userRepository = userRepository;
+        this.userProducer = userProducer;
     }
 
     @Transactional
     public UserEntity saveUser(UserDTO userDTO){
         UserEntity user = new UserEntity();
         BeanUtils.copyProperties(userDTO, user);
-        return userRepository.save(user);
+        user = userRepository.save(user);
+        userProducer.publishMessageEmail(user);
+        return user;
     }
 }
